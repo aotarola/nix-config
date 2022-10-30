2706dd83bfc7e77775b1f05e1c80e2aae501ea2c
Andres Otarola <andres@otarola.me>
2022-10-29 17:29:42 -0700

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "My Home Manager flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { home-manager, nixpkgs, ... }:
+    let
+      username = "aotarola";
+    in
+    let
+      getConfig = system: {
+        pkgs = nixpkgs.legacyPackages.${system};
+        modules = [
+          ./home.nix
+          {
+            home = {
+              username = "aotarola";
+              homeDirectory = /Users/aotarola;
+              stateVersion = "22.05";
+            };
+          }
+        ];
+      };
+    in
+    {
+      homeConfigurations."${username}@ivory" = home-manager.lib.homeManagerConfiguration (getConfig "aarch64-darwin");
+      homeConfigurations."${username}@ebony" = home-manager.lib.homeManagerConfiguration (getConfig "x86_64-darwin");
+    };
+}
---
+++ home.nix
@@ -0,0 +1,153 @@
+{ config, pkgs, lib, ... }:
+
+{
+  home = {
+
+    packages = with pkgs; [
+      lsd
+      tree
+      nodejs-16_x
+      ncdu
+      rnix-lsp
+      yaml-language-server
+      difftastic
+      tree-sitter
+      fd
+      neovim
+      pyright
+      jq
+      wget
+      btop
+      docker
+      docker-compose
+      kubectl
+      colima
+      rustup
+      elmPackages.elm-language-server
+    ];
+
+    sessionVariables = {
+      EDITOR = "hx";
+      VISUAL = "hx";
+    };
+
+    sessionPath = [
+      "$HOME/.cargo/bin"
+      "$HOME/.local/bin"
+    ];
+
+    shellAliases =
+      {
+        h = "hx";
+        gs = "git status";
+        gd = "git dft";
+        ga = "git add .";
+        gca = "git commit -v -a";
+        gc = "git commit";
+        gco = "git checkout";
+        hm = "home-manager";
+        hms = "hm switch";
+        k = "kubectl";
+        nci = "npm ci";
+        ns = "npm start";
+        ls = "lsd";
+        ll = "lsd -l";
+      };
+  };
+
+  programs.home-manager.enable = true;
+
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+
+  programs.zsh = {
+    enable = true;
+    dotDir = ".config/zsh";
+
+    oh-my-zsh =
+      {
+        enable = true;
+
+        plugins = [
+          "vi-mode"
+        ];
+      };
+
+    zplug = {
+      enable = true;
+      plugins = [
+        { name = "mafredri/zsh-async"; tags = [ from:github ]; }
+        { name = "sindresorhus/pure"; tags = [ use:pure.zsh from:github as:theme ]; }
+      ];
+
+    };
+
+  };
+
+  programs.bat = {
+    enable = true;
+    config = {
+      theme = "TwoDark";
+      italic-text = "always";
+    };
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "aotarola";
+    userEmail = "andres@otarola.me";
+    aliases = {
+      prettylog = "...";
+    };
+    extraConfig = {
+      core = {
+        editor = "hx";
+      };
+      color = {
+        ui = true;
+      };
+      pull = {
+        rebase = true;
+      };
+      fetch = {
+        prune = true;
+      };
+      pager = {
+        difftool = true;
+      };
+      diff = {
+        colorMoved = "zebra";
+        tool = "difftastic";
+      };
+      difftool = {
+        cmd = "difft \"$LOCAL\" \"$REMOTE\"";
+        prompt = false;
+      };
+      init = {
+        defaultBranch = "main";
+      };
+    };
+    ignores = [
+      ".DS_Store"
+      "*.pyc"
+      "*.log"
+      "*.pdf"
+      "*.orig"
+      "*.fls"
+      "*.fdb_latexmk"
+      "*.gz"
+      "*.sublime-project"
+      "*.sublime-workspace"
+      ".vscode/"
+      ".vim/"
+      ".vimspector.json"
+      ".marksman.toml"
+    ];
+  };
+
+  # Example of custom arbitrary dotfile
+  xdg.configFile."foo".source = ./dotfiles/foo/config.yaml;
+}
